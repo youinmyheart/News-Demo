3e5f6074c3804cba2f63016f759c019c0d11204f
Phong <tuanphong1081985@gmail.com>
2020-02-28 23:55:36 +0700

    Init NewsDemo project

---
+++ NewsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+// 
+// ViewController.swift
+// 
+// Created on 2/28/20.
+// 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
